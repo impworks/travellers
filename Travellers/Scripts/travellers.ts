95dfd8d41f2022352f7deb6281ef304165e726d0
Andrew Kourosh <mail@spark.ms>
2014-12-21 19:53:21 +0300

    Solution stub commit.

---
+++ Travellers/Scripts/travellers.ts
@@ -0,0 +1,20 @@
+﻿class TravellersGame extends Phaser.Game {
+    // -----------------------
+    // Constants
+    // -----------------------
+
+    static SCREEN_WIDTH: number = 960;
+    static SCREEN_HEIGHT: number = 640;
+
+    // -----------------------
+    // Constructor
+    // -----------------------
+    constructor() {
+        super(TravellersGame.SCREEN_WIDTH, TravellersGame.SCREEN_HEIGHT, Phaser.AUTO);
+
+//        this.state.add('Main', MainState, false);
+//        this.state.start('Main');
+    }
+}
+
+window.onload = () => new TravellersGame();
